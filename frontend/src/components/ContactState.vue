b546d145fd06f861c883634f0959fdeeb17496fa
Wei wenjie <18302010081@fudan.edu.cn>
2020-04-13 10:57:30 +0800

    contactstate page changed according to backend

--- frontend/src/components/ContactState.vue
+++ frontend/src/components/ContactState.vue
@@ -6,27 +6,24 @@ export default {
       //这个会议的状态： 审核中、已通过、已驳回
       myContactState:[
         {
-
-          tableData: [{
+            contactState:'审核中',
             FullName:'第32届全国互联网顶尖人才大会',
             ShortName:'互联网大会',
             BeginTime:'2019-10-18',
             ContributeDDL:'2019-11-12',
             ReleaseResultTime:'2019-12-3',
             Place:'Asia,ShangHai'
-          }],
-        },
-        {
-          contactState:'已通过',
-          tableData: [{
+          },
+       {
+            contactState:'已通过',
             FullName:'第3届消费者大会',
             ShortName:'消费者大会',
             BeginTime:'2016-10-18',
             ContributeDDL:'2017-9-12',
             ReleaseResultTime:'2017-12-3',
             Place:'中国'
-          }],
-        }],
+          },
+        ],
 
     }
   },
@@ -61,6 +58,9 @@ export default {
         type: 'warning'
       });
     },
+    seeDetail(){
+      this.$router.push({path: '/meetingDetail',query:{name:this.conState.FullName}});
+    }
   }
 
 
@@ -70,33 +70,50 @@ export default {
 <template>
 <div id = "ContactState">
   <el-divider content-position="left"><span>My Contribution</span></el-divider>
-  <div class="myContribution" v-for="conState in myContactState" v-bind:key="conState.contactState" v-bind:index="conState.contactState">
-    <el-table
-      :data="conState.tableData"
-      stripe
-      style="width: 100%">
-      <el-table-column prop="FullName" label="会议全称" width="150"></el-table-column>
-      <el-table-column prop="ShortName" label="会议简称" width="150"></el-table-column>
-      <el-table-column prop="BeginTime" label="举办日期" width="150"></el-table-column>
-      <el-table-column prop="ContributeDDL" label="投稿截止日期" width="150"></el-table-column>
-      <el-table-column prop="ReleaseResultTime" label="结果发布日期" width="150"></el-table-column>
-      <el-table-column prop="Place" label="举办地点" width="180"></el-table-column>
-    </el-table>
-
-<b class="state">审核状态：<span style="color: blue">{{conState.contactState}}</span></b>
-  </div>
+    <div class="MeetingBox" v-for="conState in myContactState" v-bind:key="conState.contactState" v-bind:index="conState.contactState">
+      <div class="text item">
+        <b>会议简称</b><span class="para">{{conState.ShortName}}</span>
+      </div>
+      <div class="text item">
+        <b>会议全名</b><span class="para">{{conState.FullName}}</span>
+      </div>
+      <div class="text item">
+        <b>投稿截止日期</b><span class="para">{{conState.ContributeDDL}}</span>
+      </div>
+      <div class="text item">
+        <b>结果发布日期</b><span class="para">{{conState.ReleaseResultTime}}</span>
+      </div>
+      <div class="text item">
+        <b>会议举办日期</b><span class="para">{{conState.ReleaseResultTime}}</span>
+      </div>
+      <div class="text item">
+        <b>审核状态</b><span class="para">{{conState.contactState}}</span>
+      </div>
+      <div class="text item">
+        <b>举办地点</b><span class="para">{{conState.Place}}</span>
+      </div>
+      <el-button type="primary" class="enterMeetingBt">进入会议</el-button>
+    </div>
 
 </div>
 </template>
 <style scoped>
-  .myContribution{
+  .MeetingBox{
     padding: 20px;
     border-radius: 2px;box-shadow: 0 2px 4px rgba(0, 0, 0, .44), 0 0 6px rgba(0, 0, 0, .44);color: #0c5460;color: black;
     background-color: white;
     margin: 20px;
   }
-  .state{
-    margin: 10px;
+  .text item{
+    margin-top: 20px;
+  }
+  .para{
+    margin-left: 30px;
+    font-family: "Comic Sans MS";
+    color: #c69500;
+  }
+  .enterMeetingBt{
+    margin-top: 20px;
   }
 </style>
 

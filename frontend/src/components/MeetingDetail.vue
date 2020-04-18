5cbeeec7a4b234f35a9ee9b95c41c29c176dbf28
Wei wenjie <18302010081@fudan.edu.cn>
2020-04-18 17:10:06 +0800

    judge user role in a conference

--- frontend/src/components/MeetingDetail.vue
+++ frontend/src/components/MeetingDetail.vue
@@ -93,13 +93,52 @@
     data() {
       return {
         verDetailContactData:{},
-
+        //role表示当前用户在当前会议中的角色，如果他自己是chair的话，投稿功能是禁用的
+        role:'',
       };
     },
     methods:{
       up(){
-        this.$router.push({path: '/upload',query:{name:this.$route.query.name}});
-      }
+        this.$axios.post('/judgeRoleInMeeting', {
+          //会议的全名
+          fullname:this.$route.query.name,
+          //用户的名字
+          username:this.$store.state.userDetails
+        })
+          .then(resp => {
+            if (resp.status === 200 && resp.data.hasOwnProperty("userRoleInThisConference")) {
+              this.role = resp.data.userRoleInThisConference;
+              console.log(this.role);
+              //判断该用户在该会议中的角色确定是否能投稿
+              if(this.role=='chair') {
+                this.$message({
+                  showClose: true,
+                  message: '你是该会议的会议主席，无法投稿！',
+                });
+              }
+              else{
+                this.$router.push({path: '/upload',query:{name:this.$route.query.name}});
+              }
+            } else {
+              this.$message({
+                showClose: true,
+                message: 'upload error',
+                type: 'warning'
+              });
+              console.log(resp)
+            }
+          })
+          .catch(error => {
+            if (error.response) {
+              // 请求已发出，但服务器响应的状态码不在 2xx 范围内
+              console.log(error.response)
+            } else {
+              // Something happened in setting up the request that triggered an Error
+              console.log('Error', error.message)
+            }
+            console.log(error.config);
+          })
+      },
     }
   }
 </script>

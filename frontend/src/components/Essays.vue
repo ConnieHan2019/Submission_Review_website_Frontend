7c3bc801e03bfea7d47fe594bea05acbd52c6c05
ZhangYuru <2281744227@qq.com>
2020-06-14 14:20:41 +0800

    分配失败错误码包装

--- frontend/src/components/Essays.vue
+++ frontend/src/components/Essays.vue
@@ -227,7 +227,13 @@ export default{
             })
             .catch(error => {
                 if(error.repsonse){
-                    this.$message.error('发布失败')
+                    if(error.repsonse.status===406){
+                        this.$message.error('符合要求的审稿人不足，发布失败')
+                    }
+                    else{
+                        this.$message.error('发布失败')
+                    }
+                    
                 }
                 else{
                     this.$message.error('出现了一些错误，请重试')
